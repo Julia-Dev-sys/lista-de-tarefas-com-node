ce8368aadd502a4dd98f89eaa426091a90f226b2
Julia Rocha <juliarralvim@hotmail.com>
2023-04-27 22:05:23 -0300

    integração ao banco de dados corretamente

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongoose');
 mongodb.set("strictQuery", true);
 const connectToDatabase = async () => {
-    await mongodb.createConnection(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@aula-node-exemplo-clust.ieh47uh.mongodb.net/?retryWrites=true&w=majority`,
+    await mongodb.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@aula-node-exemplo-clust.ieh47uh.mongodb.net/?retryWrites=true&w=majority`,
     (error) => {
         if(error){
             return console.log('erro ao conectar com o banco: ' , error)
