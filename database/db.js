6ce6ed46e173c9be7cb2f11101ddf978401d4bcc
Julia Rocha <juliarralvim@hotmail.com>
2023-04-28 00:26:25 -0300

    atualizações de deploy

--- database/db.js
+++ database/db.js
@@ -1,14 +1,16 @@
 const mongodb = require('mongoose');
 mongodb.set("strictQuery", true);
 const connectToDatabase = async () => {
-    await mongodb.connect(process.env.DB_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-        
-    .then(() => console.log("Conexão realizada com sucesso!"))
-    .catch((err) => console.log(err));
-    
-};
+    await mongodb.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@aula-node-exemplo-clust.ieh47uh.mongodb.net/?retryWrites=true&w=majority`,
+    (error) => {
+        if(error){
+            return console.log('erro ao conectar com o banco: ' , error)
+        }
+        return console.log('Conexão realizada com sucesso!');
+    }
+    );
+  
+
+}
 
 module.exports = connectToDatabase;
